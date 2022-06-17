dc452238bdcb6dea6d6d5884e27b9d1c132d4ecb
haywaii1 <ayotundeonasanya@yahoo.com>
2022-06-17 11:00:42 +0100

    first commit

---
+++ App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import './style.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="header-class">
+        <h1>BASIC REACT APPLICATION</h1>        
+      </header>
+    </div>
+  );
+}
+
+export default App;
